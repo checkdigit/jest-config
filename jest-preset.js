b4f35d8f8e3294b9005a7e067223c12c7c2890e7
Carl Ansley <carl.ansley@gmail.com>
2022-09-19 11:07:47 -0400

    Update to new major version of Jest (29) (#4)

--- jest-preset.js
+++ jest-preset.js
@@ -3,15 +3,15 @@
 // eslint-disable-next-line unicorn/prefer-module
 module.exports = {
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        isolatedModules: true,
+        diagnostics: false,
+      },
+    ],
   },
   collectCoverageFrom: ['<rootDir>/src/**', '!<rootDir>/src/**/*.spec.ts'],
   setupFiles: ['<rootDir>/node_modules/dotenv/config.js'],
-  globals: {
-    'ts-jest': {
-      isolatedModules: true,
-      diagnostics: false,
-    },
-  },
   testMatch: ['<rootDir>/src/**/*.spec.ts'],
 };
