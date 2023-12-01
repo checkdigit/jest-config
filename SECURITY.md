c21a8702ee608e6c583be4a70095f990cc2cc035
Carl Ansley <carl.ansley@gmail.com>
2023-11-30 19:37:08 -0500

    Typescript 5.3, Node 20 LTS (#26)

--- SECURITY.md
+++ SECURITY.md
@@ -6,8 +6,8 @@ These versions of `@checkdigit/jest-config` are currently being supported with s
 
 | Version | Supported          |
 | ------- | ------------------ |
-| \>= 3.x | :white_check_mark: |
-| \< 3.0  | :x:                |
+| \>= 4.x | :white_check_mark: |
+| \< 4.0  | :x:                |
 
 ## Reporting a Vulnerability
 
--- jest-preset.js
+++ jest-preset.js
@@ -1,12 +1,7 @@
 // jest-preset.js
 
-import path from 'node:path';
-import { promises as fs } from 'node:fs';
+import packageJson from './package.json' with { type: 'json' };
 
-// this is an ESM Javascript file, disable TypeScript-specific type checking issues
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-const packageJson = JSON.parse(await fs.readFile(path.join(process.cwd(), 'package.json'), 'utf8'));
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 const useESM = packageJson.type === 'module';
 
 export default {
