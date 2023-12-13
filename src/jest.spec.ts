e89a77c92dc15dbf96f8e368c0634e1050a02d9a
Le Cong <le.cong@checkdigit.com>
2023-12-13 10:23:28 -0500

    disable skipLibCheck, add @jest/globals, update CI configurations

--- src/jest.spec.ts
+++ src/jest.spec.ts
@@ -1,10 +1,16 @@
 // jest.spec.ts
 
+import { jest } from '@jest/globals';
+
 import checkValue from './jest.test';
 import getValue from './jest';
 
 describe('jest', () => {
   it('should work', () => {
     checkValue(getValue());
+  });
+
+  it('mock works', () => {
+    jest.useFakeTimers();
   });
 });
